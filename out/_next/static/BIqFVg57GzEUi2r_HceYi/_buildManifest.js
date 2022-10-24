6aca266d40be45a3b71851970dc26f5c2edeb203
Alex Alaniz <obyc@okaybearsyachtclub.io>
2022-10-24 10:28:37 -0400

    first commit

---
+++ out/_next/static/BIqFVg57GzEUi2r_HceYi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b45e6a8d38b19b84.css","static/chunks/pages/index-83cadbe1d041df1b.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ pages/_app.tsx
@@ -0,0 +1,34 @@
+import type { AppProps } from "next/app";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+import Head from "next/head";
+import ThirdwebGuideFooter from "../components/GitHubLink";
+import { Header } from '../components/common'
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}
+    chainRpc={{ [ChainId.Mainnet]: "https://restless-lingering-slug.quiknode.pro/5ad1193ba51bb094965a623646748316062962f7/" }}>
+      <Header />
+      <Head>
+        <title>OBYC Labs™</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="The OBYC Hazmat Division consists of 10,000 Lab Items with 7,777 L1 🧪 🐟 items being airdropped to all Okay Bear Yacht Club Bears. Choose your path wisely with L2 🧪🐟 items mint. The official snapshot was taken on 9/19/2022 at 9:19 PM EST. Instructions to follow..."
+        />
+        <meta
+          name="keywords"
+          content="OBYC Labs, Hazmat Division Edition drop"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <ThirdwebGuideFooter />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
